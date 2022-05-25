3b1605d8c49e8dab6a103c9a243c8801827fd2da
UJJWAL1711 <ujjwalrastogi17@gmail.com>
2022-05-25 08:42:29 +0530

    instant search with server side filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState} from "react";
 function App() {
+
+  const [animals, setanimals] = useState([]);
+  const search = async(q) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({q})
+    );
+    const data = await response.json();
+    setanimals(data);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => { search(e.target.value) }}
+      />
+      <ul>
+      { animals.map((animal)=>(
+        <li key={animal.id}>
+          <strong> {animal.type} </strong> {animal.name}
+        </li>
+      ))}
+      
+      {animals.length === 0 && 'No animals found'}  
+      </ul>
     </div>
   );
 }
